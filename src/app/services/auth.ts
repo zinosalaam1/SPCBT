26fa39f1633888b51ed6453d006f2be9b3294912
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-01-04 15:17:18 +0100

    first commit

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/auth`;
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const login = async (username: string, password: string) => {
   const res = await axios.post(`${API_URL}/login`, {
